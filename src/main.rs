3a77f57f942267db18b1add82571827141b0c3d9
Mikhail Subbotin <misubbotin@edu.hse.ru>
2023-11-09 01:34:01 -0300

    add echo answer

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,64 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+use itertools::Itertools;
+#[derive(Debug)]
+struct Request {
+    request_type: String,
+    path: Vec<String>,
+}
+
+fn parse_path(lines: &Vec<String>) -> Option<Request> {
+    if let Some(line) = lines.first() {
+        if let [request_type, path] = line.split_whitespace().collect_vec().as_slice()[0..=1] {
+            return Some(Request {
+                request_type: String::from(request_type),
+                path: path
+                    .split('/')
+                    .skip(1)
+                    .map(|value| String::from(value))
+                    .collect_vec(),
+            });
+        }
+    }
+    None
+}
+
+fn make_responce(request: Option<Request>) -> Option<String> {
+    if request.is_none() {
+        return None;
+    }
+    let value = request.unwrap();
+    println!("{value:?}");
+    if value.path.len() < 1 {
+        return None;
+    }
+    let main_path = value.path[0].as_str();
+    return match main_path {
+        "echo" => {
+            if let Some(v) = value.path.get(1) {
+                Some(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n{}\r\n\r\n", v.as_bytes().len(), v))
+            } else {
+                None
+            }
+        }
+        "" => Some(String::from("HTTP/1.1 200 OK\r\n\r\n")),
+        _ => None,
+    };
+}
+
 fn handle_connection(mut stream: TcpStream) {
-    let buf_readr = BufReader::new(&stream);
-    let _request: Vec<String> = buf_readr
+    let request: Vec<String> = BufReader::new(&stream)
         .lines()
         .map(|line| line.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-
-    let path_line: &str = _request
-        .iter()
-        .filter(|line| line.starts_with("GET"))
-        .next()
-        .unwrap()
-        .split_whitespace()
-        .nth(1)
-        .unwrap();
-
-    // println!("Request: {:#?}", _request);
-    // println!("Path: {:#?}", path);
-    let response = match path_line {
-        "/" => "HTTP/1.1 200 OK\r\n\r\n".as_bytes(),
-        _ => "HTTP/1.1 404 Not Found\r\n\r\n".as_bytes(),
+    let response = match make_responce(parse_path(&request)) {
+        Some(_response) => _response,
+        None => String::from("HTTP/1.1 404 Not Found\r\n\r\n"),
     };
-    if let Err(e) = stream.write_all(response) {
+
+    if let Err(e) = stream.write_all(response.as_bytes()) {
         println!("Error stream writer: {}", e);
     }
 }
