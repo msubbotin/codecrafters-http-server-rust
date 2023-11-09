bf9b221c0a0ff8470d53b6e5723a04ef3b7d2b3a
Mikhail Subbotin <misubbotin@edu.hse.ru>
2023-11-09 13:04:07 -0300

    add user-agent answer

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,30 @@ use std::{
 use itertools::Itertools;
 #[derive(Debug)]
 enum Request {
-    GET(String),
+    GET {
+        path: String,
+        user_agent: Option<String>,
+    },
     POST(String),
 }
 
 fn parse_path(lines: &Vec<String>) -> Option<Request> {
     println!("{:?}", lines);
+    let user_agent: Option<String> = lines
+        .iter()
+        .filter(|line| line.starts_with("User-Agent"))
+        .map(|line| line.split_whitespace())
+        .flatten()
+        .map(|value| String::from(value))
+        .nth(1);
     if let Some(line) = lines.first() {
         if let [request_type, path] = line.split_whitespace().collect_vec().as_slice()[0..=1] {
             let path = String::from(path.split_once('/').unwrap_or_default().1);
             return match request_type {
-                "GET" => Some(Request::GET(path)),
+                "GET" => Some(Request::GET {
+                    path: path,
+                    user_agent: user_agent,
+                }),
                 "POST" => Some(Request::POST(path)),
                 _ => None,
             };
@@ -28,17 +41,26 @@ fn parse_path(lines: &Vec<String>) -> Option<Request> {
 fn make_responce(request: Option<Request>) -> Option<String> {
     println!("{:?}", request);
     match request {
-        Some(Request::GET(path)) => {
+        Some(Request::GET { path, user_agent }) => {
             if path.starts_with("echo") {
                 match path.split_once('/') {
                     Some(("echo", other)) => Some(format!(
                     "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",
                     other.len(),
                     other
-                    
                 )),
                     None => None,
                     _ => None,
+                }
+            } else if path.starts_with("user-agent") {
+                if let Some(_user_agent) = user_agent {
+                    Some(format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",
+                        _user_agent.len(),
+                        _user_agent
+                    ))
+                } else {
+                    None
                 }
             } else if path.is_empty() {
                 Some(String::from("HTTP/1.1 200 OK\r\n\r\n"))
